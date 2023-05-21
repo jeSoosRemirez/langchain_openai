4affbcbfe169d25e9a20a5412b0312097b8650e3
jeSoosRemirez <dkhomych0091@gmail.com>
2023-05-21 21:29:07 +0300

    Create APIToken auth

---
+++ auth.py
@@ -0,0 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+from fastapi import HTTPException, Security
+from fastapi.security.api_key import APIKeyHeader
+from starlette.status import HTTP_403_FORBIDDEN
+
+load_dotenv()
+api_key_header = APIKeyHeader(name="access_token", auto_error=False)
+
+
+async def get_api_key(api_key_header: str = Security(api_key_header)):
+    """
+    Validates API KEY
+    """
+    if api_key_header == os.getenv("API_KEY"):
+        return api_key_header
+    else:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Could not validate API KEY"
+        )
